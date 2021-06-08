a8b575a2f341ecc6664b7f0bf0683615128566c8
xenogearsX <xenogears@hotmail.fr>
2021-06-08 10:27:41 +0200

    create custom hook to check token if token is present in local storage

---
+++ src/hooks/useCheckToken.js
@@ -0,0 +1,28 @@
+import { useContext, useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+import UserContext from '../context/UserContext'
+
+export const useCheckToken = () => {
+  const account = useContext(UserContext)
+
+  const checkToken = () => {
+    const token = localStorage.getItem('token')
+    axios({
+      method: 'POST',
+      url: 'http://localhost:3030/account/protected',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => {
+      if (res.data.idAccount) {
+        account[6](res.data.idAccount)
+        account[8](res.data.isAdmin)
+      }
+    })
+  }
+  useEffect(() => {
+    checkToken()
+  }, [])
+  return
+}
