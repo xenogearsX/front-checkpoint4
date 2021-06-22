51e01e8ddff416cfdf4fb234ac0b2b471f1e3ff7
xenogearsX <xenogears@hotmail.fr>
2021-06-22 15:11:03 +0200

    create userdetails component

---
+++ src/components/UserDetails.js
@@ -0,0 +1,9 @@
+const UserDetails = () => {
+  return (
+    <div>
+      <h1>Données personnelles</h1>
+    </div>
+  )
+}
+
+export default UserDetails
--- src/screen/Account.js
+++ src/screen/Account.js
@@ -1,33 +1,20 @@
-import { useEffect, useState } from 'react'
-
-import axios from 'axios'
 import { Route, Switch } from 'react-router-dom'
 
 import { useProtected } from '../hooks/useProtected'
 import NavAccount from '../components/NavAccount'
 import Welcome from '../components/Welcome'
+import UserDetails from '../components/UserDetails'
 
 const Account = () => {
   const [isLoading] = useProtected()
-  const [user, setUser] = useState(null)
-
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:3030/types')
-  //     .then(res => res.data)
-  //     .then(data => setUser(data))
-  //     .catch(e => {
-  //       console.log(`Erreur lors de la reception : ${e.message}`)
-  //     })
-  // }, [])
 
   return (
     <div className='account'>
       {isLoading ? <div>loading</div> : <NavAccount />}
       <Switch>
         <Route exact path='/account' component={Welcome} />
-        {/* <Route path='/account/details' component={UserDetails} />
-        <Route path='/account/orders' component={UserOrders} /> */}
+        <Route path='/account/details' component={UserDetails} />
+        {/* <Route path='/account/orders' component={UserOrders} /> */}
       </Switch>
     </div>
   )
