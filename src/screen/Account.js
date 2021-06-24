863f09724d693fcbe05965bf812934cb83347bda
xenogearsX <xenogears@hotmail.fr>
2021-06-24 10:33:41 +0200

    create a route for usermodif component

--- src/screen/Account.js
+++ src/screen/Account.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import { useProtected } from '../hooks/useProtected'
 import NavAccount from '../components/NavAccount'
 import UserDetails from '../components/UserDetails'
+import UserModif from '../components/UserModif'
 import UserOrders from '../components/UserOrders'
 import Welcome from '../components/Welcome'
 
@@ -16,6 +17,7 @@ const Account = () => {
         <Route exact path='/account' component={Welcome} />
         <Route path='/account/details' component={UserDetails} />
         <Route path='/account/orders' component={UserOrders} />
+        <Route path='/account/usermodif/:id' component={UserModif} />
       </Switch>
     </div>
   )
