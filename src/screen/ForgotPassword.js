097363a13b075cf0f43ce64660451acbf4af60ce
xenogearsX <xenogears@hotmail.fr>
2021-07-02 19:44:16 +0200

    create forgotpassword component

---
+++ src/screen/ForgotPassword.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+import axios from 'axios'
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const handleChange = e => setEmail(e.target.value)
+
+  const submitForm = e => {
+    e.preventDefault()
+    email === ''
+      ? setMessage('Veuillez saisir votre email.')
+      : axios
+          .post('http://localhost:3030/account/forgot', {
+            email: email
+          })
+          .then(res => {
+            setMessage(res.data)
+          })
+  }
+
+  return (
+    <div className='form'>
+      <form onSubmit={submitForm}>
+        <div className='containerAdmin'>
+          <fieldset className='formData'>
+            <legend htmlFor='email'>
+              Email<span> * </span>
+            </legend>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              placeholder='example@mail.com'
+              onChange={handleChange}
+              required
+              value={email}
+            />
+          </fieldset>
+
+          <p className='required'>
+            <span> * </span> Obligatoire
+          </p>
+          <div className='formData'>
+            <input
+              className='send'
+              type='submit'
+              value='Réinitialiser mot de passe'
+            />
+          </div>
+        </div>
+      </form>
+      {message ? <p className='message'>{message}</p> : null}
+    </div>
+  )
+}
+
+export default ForgotPassword
