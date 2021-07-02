138c3ab6d7387789ed28dcae28d0e8e3213301be
xenogearsX <xenogears@hotmail.fr>
2021-07-02 13:58:58 +0200

    change body width to 100vw + same code syntaxe as other axios in useprotected

--- src/hooks/useProtected.js
+++ src/hooks/useProtected.js
@@ -14,21 +14,21 @@ export const useProtected = () => {
 
   const protectedRoute = () => {
     const token = localStorage.getItem('token')
-    axios({
-      method: 'POST',
-      url: 'http://localhost:3030/account/protected',
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }).then(res => {
-      if (res.data.idAccount) {
-        account[6](res.data.idAccount)
-        account[8](res.data.isAdmin)
-      } else {
-        history.push('/denied')
-      }
-      setIsloading(false)
-    })
+    axios
+      .post('http://localhost:3030/account/protected', {
+        authorization: `Bearer ${token}`
+      })
+      .then(res => {
+        console.log(res)
+        if (res.data.idAccount) {
+          account[6](res.data.idAccount)
+          account[8](res.data.isAdmin)
+        } else {
+          history.push('/denied')
+        }
+        setIsloading(false)
+      })
+      .catch(e => console.log(e.response.data))
   }
   return [isLoading]
 }
