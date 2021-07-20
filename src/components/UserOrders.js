8a9fa2f38901c88a5a15bc24a1496dc2aa4ce5d1
xenogearsX <xenogears@hotmail.fr>
2021-07-20 13:22:09 +0200

    css for userorder components

--- src/components/UserOrders.js
+++ src/components/UserOrders.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 import UserContext from '../context/UserContext'
 
+import './UserOrder.css'
+
 const UserOrders = () => {
   const account = useContext(UserContext)[5]
   const [orders, setOrders] = useState([])
@@ -15,21 +17,20 @@ const UserOrders = () => {
   }, [])
 
   return (
-    <div>
+    <div id='order'>
       <h1>Vos commandes</h1>
       {orders.map(order => (
-        <div key={order.idorder}>
+        <div key={order.idorder} id='orderList'>
           <h2>
             Commande n° {order.idorder} du {order.date}
           </h2>
           {order.orderitems.map(item => (
-            <ul key={item.idproduct}>
+            <ul key={item.idproduct} className='order'>
               <li>{item.name}</li>
-              <li>{item.price}</li>
-              <li>{item.quantity}</li>
+              <li>{item.price + ' x ' + item.quantity}</li>
             </ul>
           ))}
-          <p>Total : {order.total}</p>
+          <p>Total : {order.total} €</p>
         </div>
       ))}
     </div>
